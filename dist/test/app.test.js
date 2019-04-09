53722df75ca80cd130157669a76a7362167ba547
Rachel Weil <partytimehexcellent@gmail.com>
2019-04-08 23:13:35 -0500

    Generator

--- dist/test/app.test.js
+++ dist/test/app.test.js
@@ -1,9 +1,33 @@
 "use strict";
-// import assert = require('assert');
-// import * from ('../app')
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+const fileService_1 = require("../src/fileService");
+describe('CreateProject', () => {
+    const testJsonPath = 'test/data/test.json';
+    const testFileService = new fileService_1.default(testJsonPath);
+    describe('CreateProjectFolders', () => {
+        it('should create folders for the project', () => {
+            const jsonString = fs.readFileSync(testJsonPath).toString();
+            const jsonObj = JSON.parse(jsonString);
+            const projectName = jsonObj.project;
+            for (let i = 0; i < jsonObj.scenes.length; i++) {
+                try {
+                    fileService_1.default.createDir(projectName + '/' + jsonObj.scenes[i].name + '/');
+                }
+                catch (err) {
+                    console.error(err);
+                }
+            }
+        });
+    });
+});
+//         // const testIsFileCreated = testYamlService.CreateFile();
+//         // assert.equal(testIsFileCreated, false);
+//     });
+// });
 // describe('CreateFolders', () => {
 //     it('should create folders for each scene in .JSON', () => {
-//         ensureDir("game");
+//         //fileService.ensureDir("game");
 //         // const testIsFileCreated = testYamlService.CreateFile();
 //         // assert.equal(testIsFileCreated, false);
 //     });
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,35 @@
-// import assert = require('assert');
-// import * from ('../app')
+import assert = require('assert');
+import fs = require('fs');
+import FileService from '../src/fileService';
 
+describe('CreateProject', () => {
+  const testJsonPath = 'test/data/test.json';
+  const testFileService = new FileService(testJsonPath);
+
+  describe('CreateProjectFolders', () => {
+    it('should create folders for the project', () => {
+      const jsonString: string = fs.readFileSync(testJsonPath).toString();
+      const jsonObj = JSON.parse(jsonString);
+      const projectName = jsonObj.project;
+      for (let i = 0; i < jsonObj.scenes.length; i++) {
+        try {
+          FileService.createDir(projectName + '/' + jsonObj.scenes[i].name + '/');
+        } catch (err) { 
+          console.error(err);
+        }
+      }
+    });
+  });
+});
+
+
+//         // const testIsFileCreated = testYamlService.CreateFile();
+//         // assert.equal(testIsFileCreated, false);
+//     });
+// });
 // describe('CreateFolders', () => {
 //     it('should create folders for each scene in .JSON', () => {
-//         ensureDir("game");
+//         //fileService.ensureDir("game");
 //         // const testIsFileCreated = testYamlService.CreateFile();
 //         // assert.equal(testIsFileCreated, false);
 //     });
