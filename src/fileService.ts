da065ffb6d6b321ea4fecf1492e226a8067bef26
Rachel Weil <partytimehexcellent@gmail.com>
2019-04-12 21:10:00 -0500

    Checkpoint

--- src/fileService.ts
+++ src/fileService.ts
@@ -21,33 +21,3 @@ export default class fileService {
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-// export default class fileService {
-
-//   private jsonPath: string;
-//   constructor(jsonPath: string) {
-//     this.jsonPath = jsonPath;
-//   }
-
-//   public static async createDir(path: string) {
-
-//     try {
-//       await fs.promises.mkdir(path, { recursive: true })
-//     } catch (err) {
-//       if (err.code !== 'EEXIST') throw err
-//     }
-
-//   }
-
-
-// }
